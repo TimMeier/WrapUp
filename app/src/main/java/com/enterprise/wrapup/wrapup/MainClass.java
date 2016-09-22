c39ba667331977047ecab9a80a30d51710ad5810
Gregor Stuber <gregor98@sunrise.ch>
2016-09-22 14:31:11 +0200

    Added functions in Class List

---
+++ app/src/main/java/com/enterprise/wrapup/wrapup/MainClass.java
@@ -0,0 +1,16 @@
+package com.enterprise.wrapup.wrapup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by admin on 22.09.2016.
+ */
+public class MainClass {
+    private ArrayList<List> lists;
+
+    public void changeList(List list){
+        
+    }
+
+
+}
