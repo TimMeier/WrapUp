e3eea03a1949ec62b7ac09874239201572073eb4
TimMeier <timhu@gmx.net>
2016-09-22 16:15:49 +0200

    Added location

---
+++ app/src/main/java/com/enterprise/wrapup/wrapup/LocationActivity.java
@@ -0,0 +1,16 @@
+package com.enterprise.wrapup.wrapup;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 22.09.2016.
+ */
+public class LocationActivity extends AppCompatActivity   {
+
+    public LocationActivity(){
+        this.setContentView(R.layout.location_main);
+    }
+
+
+
+}
